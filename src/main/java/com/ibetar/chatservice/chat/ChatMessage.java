26632f3143035f3c52c55b5911e11d853efc5d1b
Ilich Betancourt Rangel <ilichbr@gmail.com>
2024-04-07 16:58:52 -0300

    ChatMessage class

---
+++ src/main/java/com/ibetar/chatservice/chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.ibetar.chatservice.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
